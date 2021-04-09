e3e218b15de01acfde7d31dca9199644cf3216f8
Houms <hbrinjcar@gmail.com>
2021-04-09 10:48:23 +0100

    Additional Dependencies and Hilt

---
+++ app/src/main/java/com/hbrinj/biometricredentialspoc/application/Application.kt
@@ -0,0 +1,7 @@
+package com.hbrinj.biometricredentialspoc.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application()
